92412d159578172fdf9e8bed645205cbb48404c9
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-10-28 19:29:27 -0300

    bg-img dos Projetos feito

--- src/components/projetos/ProjetoItem.tsx
+++ src/components/projetos/ProjetoItem.tsx
@@ -1,22 +1,35 @@
 import Link from "next/link";
 import { AiOutlineRightCircle } from "react-icons/ai";
+interface Props {
+  imgUrl: string;
+}
 
-export default function ProjetoItem() {
+export default function ProjetoItem({ imgUrl }: Props) {
   return (
     <section className="w-full flex h-[25rem] items-end relative">
-      <section className="w-[50rem] h-full bg-red-700 relative">
-
-        <div className="absolute w-full h-full///pareiaqui" />
-
-        <div className="text">
-          <h1>Projeto 01</h1>
-          <h2>- Website</h2>
+      <section
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+        }}
+        className={`w-[50rem] h-full relative bg-cover bg-no-repeat bg-center`}
+      >
+        <div className="absolute w-full h-full bg-gradient opacity-50 duration-700" />
+        <div className="absolute top-12 right-[-10rem] duration-500 w-fit">
+          <h1 className="text-gree-200 text-[2.5rem] [text-shadow:_-4px_5px_22px_#11172b]">
+            Projeto 01
+          </h1>
+          <h2 className="text-bl-200 text-[2rem] font-light [text-shadow:_-4px_5px_22px_#11172b]">
+            - Website
+          </h2>
         </div>
       </section>
 
-      <button type="button">
+      <button
+        type="button"
+        className="h-16 mt-0 mr-0 mb-12 ml-20 border-none bg-transparent "
+      >
         <Link href="/projetos">
-          <a>
+          <a className="text-white text-[2rem] font-extralight flex items-center gap-[0.8rem] duration-500">
             Ver Mais <AiOutlineRightCircle />
           </a>
         </Link>
--- src/components/projetos/index.tsx
+++ src/components/projetos/index.tsx
@@ -4,7 +4,7 @@ import ProjetoItem from "./ProjetoItem";
 
 export default function Projetos() {
   return (
-    <section className="w-full flex flex-col items-center gap-20">
+    <section className="w-full flex flex-col items-center gap-20 max-[2024px]:my-28 max-[640px]:my-10">
       <Head>
         <title>Projetos</title>
       </Head>
@@ -12,7 +12,10 @@ export default function Projetos() {
       <SectionTitle title="Ultimos Projetos" />
 
       <section className="w-full flex flex-col gap-16 max-[1024px]:gap-8">
-        <ProjetoItem />
+        <ProjetoItem 
+        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgZnLpK8k4UMyYkTw2PqEefZz5cE9EcjXkoQ&usqp=CAU" />
+        <ProjetoItem 
+        imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgZnLpK8k4UMyYkTw2PqEefZz5cE9EcjXkoQ&usqp=CAU" />
       </section>
     </section>
   );
