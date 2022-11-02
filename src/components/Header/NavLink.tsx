9b6bbce31e59ed9a905dc99fdd9889eec82663f5
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-11-02 15:41:54 -0300

    Estilo do header feito, depois vou finalizar o header responsivo

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../../../styles/Header.module.css";
 
 interface PropsLi {
   title: string;
@@ -13,13 +14,13 @@ export default function NavLink({ title, path }: PropsLi) {
   const isActive = router.pathname === path;
 
   return (
-    <li>
+    <li className={styles.navLi}>
       <Link href={path}>
         <a
           className={
             isActive
-              ? "text-gree-200 hover:opacity-80"
-              : "text-gra-700 hover:opacity-80"
+              ? "text-gree-200 hover:opacity-80 after:bg-gree-200"
+              : "text-gra-700 hover:opacity-80  after:bg-gra-700"
           }
         >
           {title.toUpperCase()}
