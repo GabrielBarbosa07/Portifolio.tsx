8ae229f47c28ca65070dd93e28a2e03e9a0be979
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-11-10 17:58:47 -0300

    envio de email feito

--- src/components/FormContact/Form.tsx
+++ src/components/FormContact/Form.tsx
@@ -1,21 +1,40 @@
 import style from "../../../styles/FormContact.module.css";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import emailjs from "emailjs-com";
 
 export default function Form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  async function handleSubmit(e: FormEvent) {
-    e.preventDefault()
+  async function handleSubmit(e: any) {
+    e.preventDefault();
 
-    
+    emailjs
+      .sendForm(
+        "portifolioForm",
+        "template_n940wzp",
+        e.target,
+        "myuSVVgifW2GlXR4c"
+      )
+      .then(
+        (result) => {
+          alert("Mensagem enviada com sucesso!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setName("");
+    setEmail("");
+    setMessage("");
   }
 
   return (
     <form className={style.form} data-aos="fade-up" onSubmit={handleSubmit}>
       <input
         className={style.inputForm}
+        name="name"
         placeholder="Nome"
         required
         value={name}
@@ -23,6 +42,7 @@ export default function Form() {
       />
       <input
         className={style.inputForm}
+        name="email"
         type="email"
         placeholder="Email"
         required
@@ -31,6 +51,7 @@ export default function Form() {
       />
       <textarea
         className={style.textAreaForm}
+        name="message"
         placeholder="Mensagem"
         required
         value={message}
